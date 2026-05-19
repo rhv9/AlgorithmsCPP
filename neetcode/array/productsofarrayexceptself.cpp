dd8ac490cf3ddd7d32e59e90215b2fefd11e446f
rhv9 <rahulhasmukv@gmail.com>
2026-05-19 21:20:10 +0100

    Done productsofarrayexceptself

--- neetcode/array/productsofarrayexceptself.cpp
+++ neetcode/array/productsofarrayexceptself.cpp
@@ -51,6 +51,61 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) 
+    {
+        std::vector<int> results;
+        results.reserve(nums.size());
+
+        std::vector<int> prefix;
+        prefix.reserve(nums.size());
+
+        std::vector<int> suffix;
+        suffix.reserve(nums.size());
+
+        prefix[0] = 1;
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            prefix[i] = prefix[i-1] * nums[i-1];
+        }
+
+        suffix[nums.size() - 1] = 1;
+        for (int i = nums.size() - 2; i >= 0 ; --i)
+        {
+            suffix[i] = suffix[i+1] * nums[i+1];
+        }
+
+        for (int i = 0; i < nums.size(); ++i)
+            results.push_back(prefix[i] * suffix[i]);
+
+        return results;
+    }
+};
+
+
+class Solution3 {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) 
+    {
+        std::vector<int> results;
+        results.reserve(nums.size());
+
+        results.push_back(1);
+        
+        for (int i = 1; i < nums.size(); ++i)
+            results.push_back(results[i-1] * nums[i-1]);
+
+        int suffix = 1;
+        for (int i = nums.size() - 1; i >= 0; --i)
+        {
+            results[i] = results[i] * suffix;
+            suffix = suffix * nums[i];
+        }
+        return results;
+    }
+};
+
 
 int main()
 {
@@ -58,7 +113,7 @@ int main()
     {
         Timer timer;
         std::vector<int> nums {-1,0,1,2,3};
-        std::vector<int> results = Solution().productExceptSelf(nums);
+        std::vector<int> results = Solution3().productExceptSelf(nums);
 
         for (auto i : results)
             std::cout << i << std::endl;
