583b3533a517b1d99e2b0069e8dbc20985c9e25b
rhv9 <rahulhasmukv@gmail.com>
2026-05-31 20:21:32 +0100

    Printing vector util.

--- util/common.h
+++ util/common.h
@@ -1,3 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+
+template <typename T>
+void printVector(const std::vector<T>& vec)
+{
+    if (vec.size() == 0)
+    {
+        std::cout << "[]" << std::endl;
+        return;
+    }
+    std::cout << "[";
+
+    for (int i = 0; i < vec.size() - 1; ++i)
+        std::cout << vec[i] << ", ";
+    
+    std::cout << vec[vec.size()-1] <<"]\n";
+}
