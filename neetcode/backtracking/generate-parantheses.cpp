7966db081885f015befc9cb04f8b3dcea3e69703
rhv9 <rahulhasmukv@gmail.com>
2026-07-13 10:48:33 +0100

    generate parenthesis

--- neetcode/backtracking/generate-parantheses.cpp
+++ neetcode/backtracking/generate-parantheses.cpp
@@ -1,10 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <stdint.h>
-#include <stack>
-#include <string>
-#include <queue>
-
 #include <util/timer.h>
 using namespace std;
 
@@ -12,6 +5,30 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) 
     {
-        
+        std::string building;
+        std::vector<std::string> results;
+        generateParentehsisInternal(n, building, 0, 0, results);
+        return results;
+    }
+
+    void generateParentehsisInternal(int n, std::string& building, int open, int close, std::vector<std::string>& results)
+    {
+        if (open == n && close == n)
+        {
+            results.push_back(building);
+            return;
+        }
+        if (open < n)
+        {
+            building.push_back('(');
+            generateParentehsisInternal(n, building, open + 1, close, results);
+            building.pop_back();
+        }
+        if (close < open && close < n)
+        {
+            building.push_back(')');
+            generateParentehsisInternal(n, building, open, close + 1, results);
+            building.pop_back();
+        }
     }
 };
